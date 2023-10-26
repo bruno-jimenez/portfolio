c884a4e97a7defedc51e6ca46b1b8d61c9e31425
unknown <bruno.jimenez@laplateforme.io>
2023-10-26 14:03:09 +0200

    adding files

--- Documents/projet/Runtrack/runtrack-b2-js/Jour-01/job-04/script.js
+++
@@ -1,9 +0,0 @@
-function myArraySum(array) {
-
-let sum = 0;
-
-    for (let i = 0; i < array.length; i++) {
-        sum += array[i];
-    }
-            return sum
-}
--- Documents/projet/Runtrack/runtrack-b2-js/Jour-02/job-05/request.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$host = 'localhost';
-$dbname = 'lp_official';
-$username = 'votre_nom_utilisateur';
-$password = 'votre_mot_de_passe';
-
-?>
